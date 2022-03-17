00eca7322ce519ac55b2817e3a04b5f48f5887ae
KY0602 <mcwinmu@gmail.com>
2022-03-17 12:31:13 +0800

    Bottom Navigation Bar + Tab

---
+++ app/src/main/java/com/example/hw5/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.hw5;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
+
+import com.example.hw5.HomeFragment;
+import com.example.hw5.PostFragment;
+import com.example.hw5.SearchFragment;
+import com.example.hw5.ProfileFragment;
+import com.example.hw5.StatusFragment;
+
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+    }
+    HomeFragment homeFragment = new HomeFragment();
+    PostFragment postFragment = new PostFragment();
+    SearchFragment searchFragment = new SearchFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, homeFragment)
+                        .addToBackStack(null)
+                        .commit();
+                return true;
+            case R.id.post:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, postFragment)
+                        .addToBackStack(null)
+                        .commit();
+                return true;
+            case R.id.search:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, searchFragment)
+                        .addToBackStack(null)
+                        .commit();
+                return true;
+            case R.id.profile:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, profileFragment)
+                        .addToBackStack(null)
+                        .commit();
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onBackPressed(){
+        FragmentManager fm = getFragmentManager();
+        if (fm.getBackStackEntryCount() > 0) {
+            Log.d(LOG_TAG, "Popping backstack");
+            fm.popBackStack();
+        } else {
+            Log.d(LOG_TAG, "Nothing on backstack, calling super");
+            super.onBackPressed();
+        }
+    }
+}
